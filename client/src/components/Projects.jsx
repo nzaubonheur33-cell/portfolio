72342ca43be8e948b7c6ee01291a76f9b594c905
Bonheur <nzaubonheur84@gmail.com>
2026-09-18 23:05:34 +0200

    gdr

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { getProjects } from '../services/projects.service';
 
 function Projects() {
   const { t } = useTranslation();
