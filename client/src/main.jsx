1177c876e425ea581ee2d197d8cfe96f5e5a9fe6
Bonheur <nzaubonheur84@gmail.com>
2026-09-18 23:54:06 +0200

    jgfdjh

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './i18n';
-import '../css/input.css';
+import './index.css';
 import { ThemeProvider } from './ThemeContext';
 
 // Admin pages
